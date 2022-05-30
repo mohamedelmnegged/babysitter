8b32c336015a12648901a3e7d693b60795863346
mohamedelmnegged <mohamedaahmed92@gmail.com>
2022-05-30 10:06:40 +0200

    change dir

---
+++ Child/static/js/main.js
@@ -0,0 +1,30 @@
+const taskInput = document.getElementById('add-task-text'),
+      taskBtn = document.getElementById('add-task-input'),
+      taskWrapper = document.getElementById('tasks');
+
+taskBtn.addEventListener('click', function(){
+    let taskValue = taskInput.value;
+
+    if(taskValue == ''){
+        alert('يجب عليك ادخال نص للمهمة')
+    }else{
+        let newTaskWrapper = document.createElement('div');
+        newTaskWrapper.classList.add('task');
+        newTaskWrapper.classList.add('p-1');
+        newTaskWrapper.classList.add('mb-1');
+        
+        let newTaskcheckbox = document.createElement('input');
+        newTaskcheckbox.setAttribute('id', 'task3');
+        newTaskcheckbox.setAttribute('type', 'checkbox');
+    
+        let newTasklable = document.createElement('label');
+        newTasklable.setAttribute('for', 'task3');
+        newTasklable.innerText = taskValue;
+    
+        newTaskWrapper.append(newTaskcheckbox);
+        newTaskWrapper.append(newTasklable);
+        taskWrapper.append(newTaskWrapper);
+        taskInput.value = '';
+    }
+
+});
